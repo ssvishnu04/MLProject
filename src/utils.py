9111f6ae60a3b6d4d09f6507234e718cb290492e
SSvishnu04 <vishnu.sss.yadavalli@gmail.com>
2025-09-11 23:43:38 -0500

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import pickle
 
 from src.exception import CustomException
 from src.logger import logging
+from sklearn.metrics import r2_score
 
 def save_object(file_path,obj):
     try:
@@ -15,6 +16,28 @@ def save_object(file_path,obj):
 
         with open(file_path,'wb') as file_obj:
             dill.dump(obj,file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+
+        for i in range(len(models)):
+            model=list(models.values())[i]
+            model.fit(X_train,y_train)
+
+            y_train_pred=model.predict(X_train)
+            train_model_score=r2_score(y_train,y_train_pred) # train model score
+
+            y_test_pred=model.predict(X_test)
+
+            test_model_score=r2_score(y_test,y_test_pred)# test model score
+
+            report[list(models.keys())[i]]=test_model_score
+        
+        return report
 
     except Exception as e:
         raise CustomException(e,sys)
